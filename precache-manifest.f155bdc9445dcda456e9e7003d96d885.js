fde42d5f5ae5848e7642eef4eb455b0c20ad8e41
neekh <hv41gara@studserv.uni-leipzig.de>
2020-04-12 10:39:25 +0200

    Updates

---
+++ precache-manifest.f155bdc9445dcda456e9e7003d96d885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47004f471bfce036168b2dc631ab9aa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "849e2308a2fb22972963",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e2308a2fb22972963",
+    "url": "/portfolio/static/js/main.19348c83.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
