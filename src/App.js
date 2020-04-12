d32c1b4a5bfdc29c79a1fcffebcb9af6b2705046
neekh <hv41gara@studserv.uni-leipzig.de>
2020-04-12 12:51:19 +0200

    text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactTypingEffect from 'react-typing-effect';
 
 function App() {
+
+    const typingEffect = () => {
+        return (
+            <ReactTypingEffect
+                text="Hello. My name is Henk." //text=["Hello.", "World!"]
+            />
+        );
+    };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,14 +26,21 @@ function App() {
           <div className="DownRightDiv" ></div>
       </header>
         <body className="App-body">
-        <div className="UpLeftDiv" > </div>
-        <div className="UpRightDiv" >
-            <h1>VAN</h1>
-        </div>
-        <div className="DownLeftDiv" ></div>
-        <div className="DownRightDiv" >
-            <h1>SLOOT</h1>
-        </div>
+            <div className="App-body-NameBlocks">
+                <div className="UpLeftDiv" > </div>
+                <div className="UpRightDiv" >
+                    <h1>VAN</h1>
+                </div>
+                <div className="DownLeftDiv" ></div>
+                <div className="DownRightDiv" >
+                    <h1>SLOOT</h1>
+                </div>
+            </div>
+
+            <div className='App-body-Text'>
+                {typingEffect()}
+            </div>
+
         </body>
     </div>
   );
